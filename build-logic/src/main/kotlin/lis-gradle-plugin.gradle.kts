d8aee079a5e27b574896ee287f07991f16e38ea8
René Link <rene.link@link-intersystems.com>
2024-08-21 13:59:09 +0200

    Only apply signing work around when gradle-publish-plugin is also enabled.

--- build-logic/src/main/kotlin/lis-gradle-plugin.gradle.kts
+++ build-logic/src/main/kotlin/lis-gradle-plugin.gradle.kts
@@ -42,27 +42,30 @@ afterEvaluate {
     }
 }
 
-// Work around to allow the git-publish-plugin to coexist with the signing configuration provided by lis-gradle-plugins-maven-central-artifact
-// Otherwise we will get an error:
-//    * What went wrong:
-//    Some problems were found with the configuration of task ':git-plugin:signPluginMavenPublication' (type 'Sign').
-//    - Gradle detected a problem with the following location: 'D:\repos\git\github\link-intersystems\lis-gradle-plugins\git-plugin\build\libs\git-plugin-0.5.2-SNAPSHOT.jar.asc'.
-//
-//    Reason: Task ':git-plugin:publishMavenPublicationToSonatypeRepository' uses this output of task ':git-plugin:signPluginMavenPublication' without declaring an explicit or implicit dependency. This can lead to incorrect results being produced, depending on what order the tasks are executed.
-//
-//    Possible solutions:
-//    1. Declare task ':git-plugin:signPluginMavenPublication' as an input of ':git-plugin:publishMavenPublicationToSonatypeRepository'.
-//    2. Declare an explicit dependency on ':git-plugin:signPluginMavenPublication' from ':git-plugin:publishMavenPublicationToSonatypeRepository' using Task#dependsOn.
-//    3. Declare an explicit dependency on ':git-plugin:signPluginMavenPublication' from ':git-plugin:publishMavenPublicationToSonatypeRepository' using Task#mustRunAfter.
-//
-//    For more information, please refer to https://docs.gradle.org/8.8/userguide/validation_problems.html#implicit_dependency in the Gradle documentation.
-//
-// This error happens, because the PublishToMavenRepository tasks do not depend on the Sign tasks configured by the git-plugin.
-// So we just configure them here
-afterEvaluate {
-    tasks.withType<Sign> sign@{
-        tasks.withType<PublishToMavenRepository> {
-            dependsOn(this@sign)
+
+pluginManager.withPlugin("com.gradle.plugin-publish") {
+    // Work around to allow the git-publish-plugin to coexist with the signing configuration provided by lis-gradle-plugins-maven-central-artifact
+    // Otherwise we will get an error:
+    //    * What went wrong:
+    //    Some problems were found with the configuration of task ':git-plugin:signPluginMavenPublication' (type 'Sign').
+    //    - Gradle detected a problem with the following location: 'D:\repos\git\github\link-intersystems\lis-gradle-plugins\git-plugin\build\libs\git-plugin-0.5.2-SNAPSHOT.jar.asc'.
+    //
+    //    Reason: Task ':git-plugin:publishMavenPublicationToSonatypeRepository' uses this output of task ':git-plugin:signPluginMavenPublication' without declaring an explicit or implicit dependency. This can lead to incorrect results being produced, depending on what order the tasks are executed.
+    //
+    //    Possible solutions:
+    //    1. Declare task ':git-plugin:signPluginMavenPublication' as an input of ':git-plugin:publishMavenPublicationToSonatypeRepository'.
+    //    2. Declare an explicit dependency on ':git-plugin:signPluginMavenPublication' from ':git-plugin:publishMavenPublicationToSonatypeRepository' using Task#dependsOn.
+    //    3. Declare an explicit dependency on ':git-plugin:signPluginMavenPublication' from ':git-plugin:publishMavenPublicationToSonatypeRepository' using Task#mustRunAfter.
+    //
+    //    For more information, please refer to https://docs.gradle.org/8.8/userguide/validation_problems.html#implicit_dependency in the Gradle documentation.
+    //
+    // This error happens, because the PublishToMavenRepository tasks do not depend on the Sign tasks configured by the git-plugin.
+    // So we just configure them here
+    afterEvaluate {
+        tasks.withType<Sign> sign@{
+            tasks.withType<PublishToMavenRepository> {
+                dependsOn(this@sign)
+            }
         }
     }
 }
