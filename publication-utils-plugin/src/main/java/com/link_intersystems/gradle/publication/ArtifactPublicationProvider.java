669a80502485f322ba2ed36a6e9c20d98a74017d
René Link <rene.link@link-intersystems.com>
2024-08-29 10:45:40 +0200

    Removed unused classes.

--- publication-utils-plugin/src/main/java/com/link_intersystems/gradle/publication/ArtifactPublicationProvider.java
+++
@@ -1,9 +0,0 @@
-package com.link_intersystems.gradle.publication;
-
-import org.gradle.api.artifacts.repositories.ArtifactRepository;
-import org.gradle.api.publish.Publication;
-
-public abstract class ArtifactPublicationProvider {
-
-    public abstract ArtifactPublication tryCreateArtifactPublication(Publication publication, ArtifactRepository repository, VersionProvider versionProvider);
-}
