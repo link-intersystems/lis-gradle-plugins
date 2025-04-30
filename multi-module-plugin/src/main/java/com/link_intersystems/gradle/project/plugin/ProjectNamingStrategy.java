53068d8c32464b340c8ba9a2aed8d8bef6bbd8b9
René Link <rene.link@link-intersystems.com>
2025-04-30 12:44:45 +0200

    Bugfix.

--- multi-module-plugin/src/main/java/com/link_intersystems/gradle/project/plugin/ProjectNamingStrategy.java
+++ multi-module-plugin/src/main/java/com/link_intersystems/gradle/project/plugin/ProjectNamingStrategy.java
@@ -4,7 +4,5 @@ import java.nio.file.Path;
 
 public interface ProjectNamingStrategy {
 
-    default public String getProjectName(Path rootDirectory, Path rootRelativeProjectPath) {
-        return rootRelativeProjectPath.getFileName().toString();
-    }
+    public String getProjectName(Path rootDirectory, Path rootRelativeProjectPath);
 }
