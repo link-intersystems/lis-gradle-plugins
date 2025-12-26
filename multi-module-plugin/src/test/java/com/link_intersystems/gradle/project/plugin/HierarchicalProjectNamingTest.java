948b597ad1d8d1ce03e54b08a08f4a2401652dc7
René Link <rene.link@link-intersystems.com>
2025-12-26 06:58:56 +0100

    module.properties can override project naming.

--- multi-module-plugin/src/test/java/com/link_intersystems/gradle/project/plugin/HierarchicalProjectNamingTest.java
+++ multi-module-plugin/src/test/java/com/link_intersystems/gradle/project/plugin/HierarchicalProjectNamingTest.java
@@ -1,5 +1,7 @@
 package com.link_intersystems.gradle.project.plugin;
 
+import com.link_intersystems.gradle.project.builder.FileBuilder;
+import com.link_intersystems.gradle.project.builder.GradleSubprojectBuilder;
 import org.assertj.core.api.AbstractStringAssert;
 import org.assertj.core.api.Condition;
 import org.gradle.testkit.runner.BuildResult;
@@ -23,6 +25,9 @@ public class HierarchicalProjectNamingTest extends AbstractMultiModuleIntegratio
         });
         projectBuilder.createSubproject("moduleA/api");
         projectBuilder.createSubproject("moduleA/impl");
+        GradleSubprojectBuilder moduleBImpl = projectBuilder.createSubproject("moduleB/impl");
+        FileBuilder modulePropertiesBuilder = moduleBImpl.file("module.properties");
+        modulePropertiesBuilder.append(writer -> writer.println("name = moduleB-impl"));
         projectBuilder.createSubproject("moduleB/subB/api");
 
 
@@ -32,6 +37,7 @@ public class HierarchicalProjectNamingTest extends AbstractMultiModuleIntegratio
 
         outputAssert.contains("Including 'moduleA/api' as ':api'");
         outputAssert.contains("Including 'moduleA/impl' as ':impl'");
+        outputAssert.contains("Including 'moduleB/impl' as ':moduleB-impl'");
         outputAssert.contains("Omitting 'moduleB/subB/api'");
     }
 }
